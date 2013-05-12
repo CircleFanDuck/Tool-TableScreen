f852febfcb321e22d255e311bb19531eceb66508
CircleFanDuck <shangying05@gmail.com>
2013-05-12 22:12:52 +0800

    Support colspan header and format to jquery fn style

--- TableScreen.js
+++ TableScreen.js
@@ -40,28 +40,32 @@
 	var TableScreen = new Class;
 	
 	TableScreen.include({
-	    init: function(tableHeaderId, tableContentId, btnContentId, filterMap){
+	    init: function(headerContent, tableContent, filterMap, btnContent){
 	        var t = this;
-	        t.seqName = filterMap.x;
-	        t.headerContent = $('#'+tableHeaderId);
-	        t.header = t.headerContent.find('tr:first');
+			t.seqName = filterMap.x||filterMap.y;
+			t.headerContent = headerContent;
+			t.content = tableContent;
+			t.btnContent = btnContent;
+			t.header = t.headerContent;
 			t.header.find('td').css('position','relative');
-	        t.content = $('#'+tableContentId);
-	        t.btnContent = $('#'+btnContentId);
-	        
+			
+			t.root = $([]);
+			t.root = jQuery.merge(t.root, t.headerContent);
+			t.root = jQuery.merge(t.root, t.content);
+				        
 	        //bind hide button hover
-	        t.header.delegate('td', 'mouseenter', function(){
+	        t.headerContent.delegate('td', 'mouseenter', function(){
 	            var tip = t.addTips($(this));
 	            tip.show();
 	        });
 	        
-	        t.header.delegate('td', 'mouseleave', function(){
+	        t.headerContent.delegate('td', 'mouseleave', function(){
 	            var tip = t.addTips($(this));
 	            tip.hide();
 	        });
 	        
 	        //bind hide button
-	        t.header.delegate('.tips', 'click', function(){
+	        t.headerContent.delegate('.tips', 'click', function(){
 	            var headerOfBtn = $(this).parent();
 	            t.hide(headerOfBtn);
 	        });
@@ -69,58 +73,107 @@
 	        //bind show button
 	        t.btnContent.delegate('.tips', 'click', function(){
 	            var tipId = $(this).attr('id');
-	            var headerOfBtn = t.header.find('[tipId='+tipId+']');
+	            var headerOfBtn = t.headerContent.find('[tipId='+tipId+']');
 				t.show(headerOfBtn);
 	        });
 	    },
+		createSeq: function(header, type){
+		    var headerTr = header.find('tr');
+			//TODO: can not support colspan and rowspan together now
+			var regArr [];
+			
+		},
 	    addTips: function(headerEle){
 	        var headerTips = headerEle.find('.tips');
 	        if(headerTips.length>0){
 	            return headerTips;
 	        }
 	        var title = $.trim(headerEle.text());
-	        var id = headerEle.attr(t.seqName)+ '_' + parseInt(Math.random()*10000);
+	        var id = headerEle.attr(this.seqName)+ '_' + parseInt(Math.random()*10000);
 	        headerEle.attr('tipId', id);
 	        
 	        //TODO: use templete next time
 	        headerEle.append(
 			    '<div class="hideBtn tips" id="'+id+'">'
 			        +'<span class=hideTip style="padding: 1px 2px;font-size:0.7em;cursor:hand">x</span>'
-				    +'<span class=showTip style="display:none;padding: 1px 2px;cursor:hand">'+title+'</span>'
+				    +'<span class=showTip style="display:none;padding: 1px 3px;cursor:hand">'+title+'</span>'
 			    +'</div>');
 	        headerTips = headerEle.find('.tips');
 			headerTips.css('position', 'absolute').css('top','0').css('right',0).css('float','left');
 	        return headerTips;
 	    },
 	    findReShowTips: function(id){
-	    	var tip = t.btnContent.find('#'+id);
+	    	var tip = this.btnContent.find('#'+id);
 	    	return tip;
 	    },
 	    addReShowBtn: function(headerEle){
-	        var tip = this.addTips(headerEle).css('position', 'relative');
+	        var tip = this.addTips(headerEle).css('position', 'relative').show();
 			tip.find('.hideTip').hide();
 			tip.find('.showTip').show();
-	        t.btnContent.append(tip);
+	        this.btnContent.append(tip);
+			
+			var t = this;
+			var subHeader = t.getSubHeader(headerEle);
+			subHeader.each(function(){
+			    t.removeReShowBtn($(this));
+			})
 	    },
 	    removeReShowBtn: function(headerEle){
 	        var id = headerEle.attr('tipId');
-	        var tip = this.findReShowTips(id).css('position', 'absolute');
+	        var tip = this.findReShowTips(id).css('position', 'absolute').hide();
 			tip.find('.hideTip').show();
 			tip.find('.showTip').hide();
 	        headerEle.append(tip);
+			
+			//remove sub reshow btn
+			var t = this;
+			var subHeaders = t.getSubHeader(headerEle);
+			subHeaders.each(function(){
+			    t.removeReShowBtn($(this))
+			})
 	    },
+		getParentHeader: function(attrVal){
+		    var index = attrVal.lastIndexOf('_');
+			if(index >= 0){
+				var attrName = this.seqName;
+			    var parentAttrVal = attrVal.substring(0, index);
+				var parentHeader = this.headerContent.find('['+attrName+'='+parentAttrVal+']');
+				return parentHeader;
+			}
+			return null;
+		},
+		getSubHeader: function(parentHeader){
+	        var attrName = this.seqName;
+		    var attrVal = parentHeader.attr(attrName);
+			var visibleSubHeader = this.headerContent.find('['+attrName+'^='+attrVal+'_]');
+			return visibleSubHeader;
+		},
 	    findOperationArea: function(headerEle){
 	        var attrName = this.seqName;
 		    var attrVal = headerEle.attr(attrName)
 		    //type=filter <- single
-		    //type^=filter. <- multiple
-			var eles = jQuery.merge(t.headerContent, t.content).find('['+attrName+'='+attrVal+'], ['+attrName+'^='+attrVal+'.]');
+		    //type^=filter_ <- multiple
+			var t = this;
+			var eles = t.root.find('['+attrName+'='+attrVal+'], ['+attrName+'^='+attrVal+'_]');
 			
 			return eles;
 	    },
+		suitParentHeader: function(headerEle){
+			//check parent
+		    var attrVal = headerEle.attr(this.seqName)
+			var parentHeader = this.getParentHeader(attrVal);
+			if(parentHeader&&parentHeader.length>0){
+				var subHeaderVisible = this.getSubHeader(parentHeader).filter(':visible');
+				if(subHeaderVisible.length==0){
+					this.hide(parentHeader);
+				}
+			}
+			
+		},
 		hide: function(headerEle){
 			var eles = this.findOperationArea(headerEle).addClass('hideByTableControl');
 		    this.addReShowBtn(headerEle);
+			this.suitParentHeader(headerEle);
 		},
 		show: function(headerEle){
 			var eles = this.findOperationArea(headerEle).removeClass('hideByTableControl');
@@ -128,8 +181,9 @@
 		}
 	});
 	
-	exports.TableScreen = function(tableHeaderId, tableContentId, btnContentId, filterMap){
-	    var t = new TableScreen(tableHeaderId, tableContentId, btnContentId, filterMap);
-	    return t;
+	exports.prototype.headOf = function(tableContent, filterMap, btnContent){
+	    var headerContent = $(this);
+	    var t = new TableScreen(headerContent, tableContent, filterMap, btnContent);
+	    return headerContent;
 	}
-})(window);
+})($);
